7b582b8e77a55b136c2d6b94aa66fc6ea0044599
sysdumb32 <sysdumb32@gmail.com>
2022-07-26 03:12:00 +0000

    nixos.sh

---
+++ nixos.sh
@@ -0,0 +1,12 @@
+read -p 'This script will replace your configuration.nix, proceed? (y\n)' prompt
+
+if [ $prompt = "y" ]
+then
+	read -p "What is the hostname? " name
+
+	sed "s/nixos/$name/g" configuration.nix
+
+	rm /etc/nixos/configuration.nix
+	cp ./configuration.nix /etc/nixos/configuration.nix
+fi
+
