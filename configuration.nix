a2dc4dbbdd8ee50f9449199eecde21154bec1ae9
sysdumb32 <109949401+sysdumb@users.noreply.github.com>
2022-07-25 19:50:35 -0500

    Create configuration.nix

---
+++ configuration.nix
@@ -0,0 +1,52 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/sda";
+
+  networking.hostName = "nixos"; 
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "/";
+
+  users.users.admin = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+  };
+
+
+  environment.systemPackages = with pkgs; [
+    vim_configurable
+    git
+    wget
+    # firefox
+  ];
+
+  console.useXkbConfig = true;
+  services.xserver = {
+    enable = true;
+    # desktopManager.plasma5.enable = true;
+    xkbOptions = "ctrl:swapcaps";
+  };
+
+  # List services that you want to enable:
+
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  system.stateVersion = "22.05";
+
+}
